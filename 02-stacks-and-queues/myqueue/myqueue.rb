04565b209fb614881c1b505f94b38bd2539912e4
Meir Taffel <artgardening@gmail.com>
2017-06-15 15:33:01 +0000

    complete assignment 2

--- 02-stacks-and-queues/myqueue/myqueue.rb
+++ 02-stacks-and-queues/myqueue/myqueue.rb
@@ -5,14 +5,24 @@ class MyQueue
   def initialize
     @queue = Array.new
     @head = @queue[0]
+    @tail = @queue[0]
   end
 
   def enqueue(element)
+    @queue[@queue.size] = element
+    @head = @queue.first
+    @tail = @queue.last
   end
 
   def dequeue
+    temp = @head
+    @queue = @queue[1..-1]
+    @head = @queue.first
+    @tail = @queue.last
+    temp
   end
 
   def empty?
+    @queue == [] ? true : false
   end
 end
--- 02-stacks-and-queues/mystack/mystack.rb
+++ 02-stacks-and-queues/mystack/mystack.rb
@@ -7,11 +7,18 @@ class MyStack
   end
 
   def push(item)
+    @stack[@stack.size] = item
+    self.top = @stack.last
   end
 
   def pop
+    temp = @stack.last
+    @stack = @stack[0...-1]
+    self.top = @stack.last
+    temp
   end
 
   def empty?
+    @stack == [] ? true : false
   end
 end
