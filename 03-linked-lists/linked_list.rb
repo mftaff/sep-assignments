1cb3d5464b41131198f62eb25fcae424836b946a
Meir Taffel <artgardening@gmail.com>
2017-06-18 14:04:21 +0000

    implement linked list

--- 03-linked-lists/linked_list.rb
+++ 03-linked-lists/linked_list.rb
@@ -6,25 +6,79 @@ class LinkedList
 
   # This method creates a new `Node` using `data`, and inserts it at the end of the list.
   def add_to_tail(node)
+    if not @head
+      @head = node
+      @tail = @head
+    else
+      temp = @head
+      while @head
+        if not @head.next
+          @head.next = node
+          @tail = @head.next
+          break
+        end
+        @head = @head.next
+      end
+      @head = temp
+    end
   end
 
   # This method removes the last node in the lists and must keep the rest of the list intact.
   def remove_tail
+    if @head == @tail
+      @head = nil
+      @tail = nil
+    else
+      temp = @head
+      while @head
+        if @head.next.next == nil
+          @head.next = nil
+          @tail = @head
+          break
+        else
+          @head = @head.next
+        end
+      end
+      @head = temp
+    end
   end
 
   # This method prints out a representation of the list.
   def print
+    node = @head
+    while node 
+      puts node.data
+      node = node.next
+    end
   end
 
   # This method removes `node` from the list and must keep the rest of the list intact.
   def delete(node)
+    if @head == node
+      @head = @head.next
+    else
+      temp = @head
+      while @head
+        if @head.next == node
+          @head.next = node.next
+          @tail = @head if node.next == nil
+          break
+        end
+        @head = @head.next
+      end
+      @head = temp
+    end
   end
 
   # This method adds `node` to the front of the list and must set the list's head to `node`.
   def add_to_front(node)
+      temp = @head
+      @head = node
+      @head.next = temp
   end
 
   # This method removes and returns the first node in the Linked List and must set Linked List's head to the second node.
   def remove_front
+    @head = @head.next if @head
   end
 end
