14aa855a060165a5765118266cfd06346ddac54e
Meir Taffel <artgardening@gmail.com>
2017-07-03 10:05:57 +0000

    fix recursive search

--- Algorithms/02-search/binary_search_recursive.rb
+++ Algorithms/02-search/binary_search_recursive.rb
@@ -1,17 +1,16 @@
-# given array and target value, returns that value from array or nil
-def recursive_binary(array, target)
-    low = 0
-    high = array.length - 1
+# given array and target value, returns target value's index or nil
+def recursive_binary(array, target, high=array.length-1, low=0)
     mid = (low+high) / 2
     
     if low <= high
-        return array[mid] if array[mid] == target
-        
+        return mid if array[mid] == target
+
         if array[mid] > target
-            recursive_binary(array[low...mid], target)
+            high = mid - 1
         elsif array[mid] < target
-            recursive_binary(array[mid+1..high], target)
+            low = mid + 1
         end
+        recursive_binary(array, target, high, low)
     end
 end
 
@@ -19,7 +18,6 @@ end
 
 arr = "a b c d e f g h i j k l m n o p".split
 
-puts recursive_binary(arr, "a")
 puts recursive_binary(arr, "b")
 puts recursive_binary(arr, "h")
 puts recursive_binary(arr, "i")
